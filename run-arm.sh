408a7caa8d50f4bec5fa5dbd42f0069df2f8d2f9
Vernon Liu <mrvernonliu@gmail.com>
2020-06-07 01:36:00 -0700

    sample c file and automation for compiling/running on arm and compiling and displaying assembly

---
+++ run-arm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+keypath=$1
+username=$2
+armuser=$3
+mode=$4
+filename=$5
+
+# Make sure you end your commands with a semicolon
+commands=()
+commands+="cd /tmp/${username};"
+
+if [[ "$mode" == "-c" ]]; then
+    commands+="echo 'compiling...';"
+    commands+="arm-linux-gcc -static -o ${username} $filename;"
+    commands+="echo 'transferring to arm...';"
+    commands+="lftp -c \"open -u ${armuser},q6coHjd7P user1@arm; put -O . ./${username}\";"
+    commands+="eval \"{ sleep 1; echo ${armuser};\
+        sleep 1; echo q6coHjd7P;\
+        echo 'chmod 777 ${username}';\
+        echo './${username}';\
+        sleep 1; echo 'rm -f ${username}'; }\"\
+        | telnet arm;"
+elif [[ "$mode" == "-s" ]]; then
+    commands+="arm-linux-gcc -static -S -o ${username} $filename;"
+    commands+="cat ${username};"
+fi
+
+#cleanup
+commands+="echo 'cleanup';"
+commands+="cd ..; rm -Rf ${username};"
+
+echo "$commands"
+
+ssh -i $keypath $username@seng440.ece.uvic.ca "cd /tmp; mkdir ${username}"
+scp -i $keypath $filename $username@seng440.ece.uvic.ca:/tmp/$username
+ssh -i $keypath $username@seng440.ece.uvic.ca "$commands"
